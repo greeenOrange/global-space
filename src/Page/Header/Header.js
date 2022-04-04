c2956f60f1c4df98ce5d554d7f9218970da9bc79
leader with sadiq <gm1sadiq@gmail.com>
2022-04-04 15:32:48 +0600

    first commit

---
+++ src/Page/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className=''>
+  <div class="content">
+            <nav class="navbar navbar-expand-lg navbar-light container">
+    <a class="navbar-brand" href="#">
+    <img src='https://i.ibb.co/StCT8fH/logo.png' alt="logo" width="100" height="50" />
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link">Story</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">blog</a>
+        </li>
+      </ul>
+          <button>Contact Us</button>
+    </div>
+</nav>
+  </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Page/HeroSection/HeroSection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './HeroSection.css'
+
+const HeroSection = () => {
+    return (
+        <main>
+            <div className="hero-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                        <div className="left-hero">
+                        <div className="welcome-section">
+                            <img src="https://i.ibb.co/6Hsq34r/r8-2.png" alt="" />
+                            <h5>Welcome to Stella</h5>
+                        </div>
+                        <h1>Space For Everyone</h1>
+                        <p>At Piap space, we belive that we can make Space and Earth cleaner, engineering, and delivering top-quality robotics solutions</p>
+                        <button>Get start</button>
+                        </div>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="right-hero">
+                                <div className="main-globe">
+                                    {/* <img src="https://i.ibb.co/rt9pr6n/space-man.png" alt="" /> */}
+                                    <img src="https://i.ibb.co/WkzW9ym/r8-1.png" alt="" />
+                                </div>
+                                <div className="space-man">
+                                    <img src="https://i.ibb.co/rt9pr6n/space-man.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default HeroSection;
