0ba9e2f1292f74f833c8f2f24d8d5b92d8a5a137
leader with sadiq <gm1sadiq@gmail.com>
2022-04-06 01:09:53 +0600

    footer added

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -7,14 +7,25 @@ const Footer = () => {
         <div className="container">
             <div className="row">
             <h1>Newsletter</h1>
-            <div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="button">Button</button>
-  </div>
-</div>
-                <div className="col-md-4">
 
+            <div class="input-group mb-3">
+        <input type="text" placeholder="Email address" aria-label="Email address" aria-describedby="basic-addon2" />
+        <button>Subscribe</button>
+            </div>
+                <div className='d-flex align-items-center justify-content-between mt-md-5'>
+                <div className="footer-logo">
+                <img src='https://i.ibb.co/StCT8fH/logo.png' alt="logo" width="100" height="50" />
+                </div>
+                <div className="footer-copyright">
+                <p>Space &copy; 2020,All rights reserve.</p>
+                </div>
+                <div className="social-media">
+                <ul className='social-section'>
+                    <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-linkedin"></i></a></li>
+                </ul>
+                </div>
                 </div>
             </div>
         </div>            
