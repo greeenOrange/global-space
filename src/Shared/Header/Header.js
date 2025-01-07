107f5239ba77dbaaafa2a20ffd6008de072f9798
greeenOrange <gm1sadiq@gmail.com>
2025-01-08 02:34:41 +0600

    adjusteksd

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
   <div className="content">
             <nav className="navbar navbar-expand-lg navbar-light container">
-    <a className="navbar-brand" href="#">
+    <NavLink className="navbar-brand" href="/" >
     <img src='https://i.ibb.co/StCT8fH/logo.png' alt="logo" width="100" height="50" />
-    </a>
+    </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
