1342657dc59369e6cb13595d756ea9221d7b6ada
greeenOrange <gm1sadiq@gmail.com>
2025-01-08 02:18:24 +0600

    adjusteksd

--- src/Page/Rockets/Rockets.js
+++ src/Page/Rockets/Rockets.js
@@ -1,5 +1,3 @@
-import { useState, useEffect, useMemo } from "react";
-import axios from "axios";
 import "./Rockets.css";
 import LaunchList from "./LaunchList/LaunchList";
 
@@ -10,42 +8,6 @@ const Rockets = () => {
       <div className="container">
         <div className="row">
         <LaunchList />
-          {/* <div className="search-container">
-            <div className="d-flex align-items-center justify-flex-start">
-              <div className="search-bar w-50">
-                <p>Is upcoming?</p>
-                <select aria-label="Default select example" value="">
-                  <option value={"select one"}>select one</option>
-                  <option value="true">yes</option>
-                  <option value="false">No</option>
-                </select>
-              </div>
-              <div className="launch-year">
-                <p>Launch Year?</p>
-                {
-                  <select
-                    aria-label="Default select example"
-                    className="w-50"
-                    value=""
-                  >
-                    <option value={"select year"}></option>
-                   
-                  </select>
-                }
-              </div>
-            </div>
-            <div className="search-input">
-              <form className="d-flex">
-                <input
-                  type="text"
-                  id="search"
-                  placeholder="Search for rocket"
-                />
-                <button type="submit">Search</button>
-              </form>
-            </div>
-          </div> */}
-
         </div>
       </div>
     </div>
